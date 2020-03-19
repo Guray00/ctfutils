1d343fba19277218c632d5f5877698a9603e61e2
Marco <30523962+Guray00@users.noreply.github.com>
2020-03-19 23:44:57 +0100

    fixed

--- install.sh
+++ install.sh
@@ -1,26 +1,22 @@
 mkdir -p ~/.ctfutils
 cp  dec2hex ~/.ctfutils
-cp  hex2dec ~/.csutils
-cp  d2h ~/.ctfutils
-cp  h2d ~/.ctfutils
+cp  hex2dec ~/.ctfutils
 cp  cuint ~/.ctfutils
 
 chmod a+x ~/.ctfutils/dec2hex
 chmod a+x ~/.ctfutils/hex2dec
-chmod a+x ~/.ctfutils/d2h
-chmod a+x ~/.ctfutils/h2d
 chmod a+x ~/.ctfutils/cuint
 
 
 if grep -Fxq 'export PATH=$PATH":$HOME/.ctfutils"' ~/.profile
 then
 	# code if found
-	echo "already add to .profile."
+	echo "already added to .profile."
 	source ~/.profile
 	
 else
     	# code if not found
-	echo 'export PATH=$PATH":$HOME/ctfutils"' >> ~/.profile
+	echo 'export PATH=$PATH":$HOME/.ctfutils"' >> ~/.profile
 	source ~/.profile
 fi
 
